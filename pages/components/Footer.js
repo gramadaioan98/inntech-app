53231b2a950efa13439ad03316c5a213080e864e
Gramada Ioan <gramadaioan98@gmail.com>
2022-04-21 16:40:40 +0300

    change name

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -6,8 +6,8 @@ const Footer = () =>{
     <footer className="bg-violet-col text-white pt-60px sm:pt-107px">
       <div className="flex max-w-1640px m-auto w-full flex-col lg:flex-row px-15px">
         <div className="social basis-2/5 gap-16 flex mb-5 lg:mb-0 justify-center">
-          <Image src="/facebook.svg" alt="facebook" width={16} height={30} />
-          <Image src="/instagram.svg" alt="instagram" width={30} height={30} />
+          <Image src="/Facebook.svg" alt="facebook" width={16} height={30} />
+          <Image src="/Instagram.svg" alt="instagram" width={30} height={30} />
           <Image src="/linkedin.svg" alt="linkedin" width={30} height={30} />
         </div>
         <div className="flex basis-3/5 justify-between pb-60px gap-5 flex-col sm:flex-row">
