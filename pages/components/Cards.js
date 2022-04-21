5eb7a73a432ce92f7556cf1ce3c4f715920fdd0b
Gramada Ioan <gramadaioan98@gmail.com>
2022-04-21 16:36:41 +0300

    change class

--- pages/components/Cards.js
+++ pages/components/Cards.js
@@ -9,7 +9,7 @@ const  Cards = () =>{
   .then(responseData => {
     data = responseData.map(function(todo){
       return (
-        <div class="card basis-full sm:basis-1/2 lg:basis-1/3 xslg:basis-1/4  p-5 flex flex-col gap-2 m-x-10" key={todo.id}>
+        <div className="card basis-full sm:basis-1/2 lg:basis-1/3 xslg:basis-1/4  p-5 flex flex-col gap-2 m-x-10" key={todo.id}>
           <div className="border-2 p-5">
           <h4 className="font-bold xslg:text-lg ">Name: {todo.name}</h4>
           <p>Email: {todo.email}</p>
